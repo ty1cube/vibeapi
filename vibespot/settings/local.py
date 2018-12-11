308d0e8c5c36e13202a3d0977f6e44081e517658
Azeez Akinsola <harkzed@gmail.com>
2018-12-11 10:52:11 +0100

    changes to the views file

--- vibespot/settings/local.py
+++ vibespot/settings/local.py
@@ -146,24 +146,24 @@ WSGI_APPLICATION = 'vibespot.wsgi.application'
 # 
 
 
-DATABASES = {
-       'default': {
-           'ENGINE': 'django.db.backends.postgresql_psycopg2',
-           'NAME': 'vibe',
-           'USER': 'dimillion',
-           'PASSWORD': 'admin',
-           'PORT': '5432',
-           'HOST':'localhost',
-           }
-       }
-
-
 # DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME':  os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
+#        'default': {
+#            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#            'NAME': 'vibe',
+#            'USER': 'dimillion',
+#            'PASSWORD': 'admin',
+#            'PORT': '5432',
+#            'HOST':'localhost',
+#            }
+#        }
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME':  os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
 
 
 
